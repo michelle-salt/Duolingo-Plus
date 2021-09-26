11616fbb2e7223c78e90a6bf578430a911ca031a
michelle-salt <cookies10m@gmail.com>
2021-09-25 20:15:09 -0400

    Updated files

--- main.js
+++ main.js
@@ -7,10 +7,14 @@ function replaceText(fromString, toString){
     var node_list = [];
 
     (function scan(node){
+      //If there's a word (length != 0)
       if(node.childNodes.length) 
+        //Loop through all words
         for(var i = 0; i < node.childNodes.length; i++) {
+          //Scan the word
           scan(node.childNodes[i]);
 		}
+      //If it's a word???
       else if(node.nodeType == Node.TEXT_NODE) {
         node_list.push(node);
 	  }
