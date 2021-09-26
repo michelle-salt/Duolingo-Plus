ecec26e0bf7cca2164b500a30fb7afe9f8b86f30
michelle-salt <cookies10m@gmail.com>
2021-09-25 20:14:10 -0400

    Translates letter by letter

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+# bilibili_translate_chrome_extension
+Chrome extension to translate bilibili into English
+
+# Word from the Developer
 # Hackapalooza2021
 The chrome extension I created for the Hackapalooza hackathon 2021
+
+# How to Install
+1. Open chrome, open extension menu from Settings or type "chrome://extensions/" to address bar.
+2. Turn on "Developer mode"
+3. Press "Load unpacked" and select extracted folder. (You have to select whole folder)
+4. Press refresh icon (No need, just in case).
+5. Turn off "Developer mode".
---
+++ main.js
@@ -0,0 +1,32 @@
+function replaceText(fromString, toString){
+  getTextNodes().forEach(function(node){
+    node.nodeValue = node.nodeValue.replace(new RegExp(quote(fromString), 'g'), toString);
+  });
+
+  function getTextNodes(){
+    var node_list = [];
+
+    (function scan(node){
+      if(node.childNodes.length) 
+        for(var i = 0; i < node.childNodes.length; i++) {
+          scan(node.childNodes[i]);
+		}
+      else if(node.nodeType == Node.TEXT_NODE) {
+        node_list.push(node);
+	  }
+    })(document);
+
+    return node_list;
+  }
+
+  function quote(str){
+    return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  }
+}
+
+setTimeout(function () {
+	for (let key in dictionary) {
+		replaceText(dictionary[key], key);
+	}
+}, 3000);
+
